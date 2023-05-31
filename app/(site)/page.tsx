0fe2d2a9816cbcddd71c7183c0a8f263b4d36e0c
William Robersone <william.robersone@gmail.com>
2023-05-30 21:22:32 -0500

    first commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+import AuthForm from "./components/AuthForm"
+
+export default function Home() {
+    return (
+      <div className="flex min-h-full flex-col justify-center py-12 bg-amber-100 sm:px-6 lg:px-8">
+        <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+          <Image alt="Logo" height="48" width="48" className="mx-auto w-auto" src="/images/message.png" />
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign Into Your Account</h2>
+        </div>
+
+        <AuthForm />
+      </div>
+    )
+  }
+  
